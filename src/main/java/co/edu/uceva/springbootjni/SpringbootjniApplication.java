f1863f66e49d4b0af6dcd56c9d4d7397ca77c44f
Jorge Eduardo Cobo Ocampo <jorge.cobo01@uceva.edu.co>
2024-11-24 13:08:09 -0500

    Inicio del proyecto

---
+++ src/main/java/co/edu/uceva/springbootjni/SpringbootjniApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.springbootjni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootjniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uceva/springbootjni/SpringbootjniApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.springbootjni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
